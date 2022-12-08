ac9cf4af78e9bfb0384d7074bdda6673e2622c63
DorisMmmm <116881280+DorisMmmm@users.noreply.github.com>
2022-12-08 19:14:35 +0100

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -8,20 +8,24 @@ class TranslatorTestClass(unittest.TestCase):
 
     def test_english_to_french(self):
         ''' test for English to French'''
-        self.assertEqual(english_to_french(""), "")
         engl = english_to_french('Hello')
         frnc = french_to_english(engl)
         self.assertEqual(frnc, 'Bonjour')
 
+    def test_english_to_french_not_equal(self):
+        ''' test for English to French'''
+        self.assertNotEqual(english_to_french("xxx"), "")
+
     def test_french_to_english(self):
         '''tests for French to English'''
-
-        self.assertEqual(french_to_english(""), "")
         frnc = french_to_english('Bonjour')
         engl = english_to_french(frnc)
         self.assertEqual(engl, 'Hello')
 
+    def test_french_to_english_not_equal(self):
+        '''tests for French to English'''
+        self.assertNotEqual(french_to_english("xxx"), "")
 
 if __name__ == '__main__':
     unittest.main()
-    
+   
