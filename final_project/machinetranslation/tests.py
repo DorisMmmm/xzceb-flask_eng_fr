5145efcdbdda51e4747ab4d436c64d1c2082ed98
DorisMmmm <116881280+DorisMmmm@users.noreply.github.com>
2022-12-08 19:13:59 +0100

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,44 +1,31 @@
-'''French English translator'''
+'''Unit tests for translator.py'''
+import unittest
+import translator
 
-import os
-import json
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from dotenv import load_dotenv
+from translator import french_to_english, english_to_french
 
+class TranslatorTestClass(unittest.TestCase):
 
-load_dotenv()
+    def test_english_to_french(self):
+        ''' test for English to French'''
+        engl = english_to_french('Hello')
+        frnc = french_to_english(engl)
+        self.assertEqual(frnc, 'Bonjour')
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+    def test_english_to_french_not_equal(self):
+        ''' test for English to French'''
+        self.assertNotEqual(english_to_french("xxx"), "")
 
-authenticator = IAMAuthenticator('qlqbtXDGtPD63iRGO7pOMy_2E3sD0mTGgUOeGdIJ9LX7')
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
+    def test_french_to_english(self):
+        '''tests for French to English'''
+        frnc = french_to_english('Bonjour')
+        engl = english_to_french(frnc)
+        self.assertEqual(engl, 'Hello')
 
-language_translator.set_service_url('https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/55694f8c-2aa6-488a-84c1-79821fa6aa3d')
+    def test_french_to_english_not_equal(self):
+        '''tests for French to English'''
+        self.assertNotEqual(french_to_english("xxx"), "")
 
-def english_to_french(english_text):
-    '''Takes English input text and returns French text'''
-    french_text = from_to(english_text, 'fr-en')
-    return french_text
-
-def french_to_english(french_text):
-    '''Takes French input text and returns English text'''
-    english_text1= from_to(french_text, 'en-fr')
-    return english_text1
+if __name__ == '__main__':
+    unittest.main()
     
-def from_to(input_text, mymodel_id):
-    '''Takes input text and returns translated text'''
-    result_text = ''
-    if input_text != '':
-        result = language_translator.translate(
-        text=input_text,
-        model_id=mymodel_id).get_result()
-        dump_json= json.dumps(result, indent=2, ensure_ascii=False)
-        result_dict = json.loads(dump_json)
-        result_translations= result_dict['translations'][0]
-        result_text= result_translations['translation']
-    return result_text
