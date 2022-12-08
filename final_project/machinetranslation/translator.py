37fccfb25e51bf67753165ff3b531c3c2c5fdee4
DorisMmmm <116881280+DorisMmmm@users.noreply.github.com>
2022-12-08 19:13:23 +0100

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -12,13 +12,13 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('url')
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/55694f8c-2aa6-488a-84c1-79821fa6aa3d')
+language_translator.set_service_url('url')
 
 def english_to_french(english_text):
     '''Takes English input text and returns French text'''
@@ -27,16 +27,15 @@ def english_to_french(english_text):
 
 def french_to_english(french_text):
     '''Takes French input text and returns English text'''
-    english_text1= from_to(french_text, 'en-fr')
-    return english_text1
+    english_text = from_to(french_text, 'en-fr')
+    return english_text
     
 def from_to(input_text, mymodel_id):
     '''Takes input text and returns translated text'''
     result_text = ''
     if input_text != '':
-        result = language_translator.translate(
-        text=input_text,
-        model_id=mymodel_id).get_result()
+        lang_tr = language_translator.translate(text=input_text, model_id=mymodel_id)
+        result = lang_tr.get_result()
         dump_json= json.dumps(result, indent=2, ensure_ascii=False)
         result_dict = json.loads(dump_json)
         result_translations= result_dict['translations'][0]
