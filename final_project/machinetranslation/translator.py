c9a85e09787246ff32eb73409e2bb4449871fa92
DorisMmmm <116881280+DorisMmmm@users.noreply.github.com>
2022-12-09 15:21:42 +0100

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,8 @@
-'''French English translator'''
-
+""" Machine Translation with Watson """
 import os
-import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -18,26 +15,24 @@ language_translator = LanguageTranslatorV3(
     authenticator=authenticator
 )
 
-language_translator.set_service_url('url')
+language_translator.set_service_url(url)
 
 def english_to_french(english_text):
-    '''Takes English input text and returns French text'''
-    french_text = from_to(english_text, 'fr-en')
+    """ Translate text from English to French """
+    if english_text is None:
+        return None
+    translation = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    french_text=translation["translations"][0]["translation"]
     return french_text
 
 def french_to_english(french_text):
-    '''Takes French input text and returns English text'''
-    english_text = from_to(french_text, 'en-fr')
+    """ Translate text from French to English """
+    if french_text is None:
+        return None
+    translation = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    english_text=translation["translations"][0]["translation"]
     return english_text
-    
-def from_to(input_text, mymodel_id):
-    '''Takes input text and returns translated text'''
-    result_text = ''
-    if input_text != '':
-        lang_tr = language_translator.translate(text=input_text, model_id=mymodel_id)
-        result = lang_tr.get_result()
-        dump_json= json.dumps(result, indent=2, ensure_ascii=False)
-        result_dict = json.loads(dump_json)
-        result_translations= result_dict['translations'][0]
-        result_text= result_translations['translation']
-    return result_text
